0ae84bc9c240791b919647493722536fc6437f40
Danang Galuh Tegar Prasetyo <danang.galuh.t@gmail.com>
2017-10-21 22:59:18 +0700

    Upgrade to ES6

--- crypto.js
+++ crypto.js
@@ -1,56 +1,49 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var crypto_js_1 = require("crypto-js");
-var SimpleCrypto = /** @class */ (function () {
-    function SimpleCrypto(secret) {
+import CryptoJS from 'crypto-js';
+export default class SimpleCrypto {
+    constructor(secret) {
         if (secret === void 0)
-            throw new Error("SimpleCrypto object MUST have a secret.");
+            throw new Error(`SimpleCrypto object MUST have a secret.`);
         this.secret = secret;
         this.keySize = 256;
         this.iterations = 100;
     }
-    SimpleCrypto.prototype.encrypt = function (string) {
-        var salt = SimpleCrypto.generateRandom(128, true);
-        var key = crypto_js_1.default.PBKDF2(this.secret, salt, {
+    encrypt(string) {
+        let salt = SimpleCrypto.generateRandom(128, true);
+        let key = CryptoJS.PBKDF2(this.secret, salt, {
             keySize: this.keySize / 32,
             iterations: this.iterations
         });
-        var initialVector = SimpleCrypto.generateRandom(128, true);
-        var encrypted = crypto_js_1.default.AES.encrypt(string, key, {
+        let initialVector = SimpleCrypto.generateRandom(128, true);
+        let encrypted = CryptoJS.AES.encrypt(string, key, {
             iv: initialVector,
-            padding: crypto_js_1.default.pad.Pkcs7,
-            mode: crypto_js_1.default.mode.CBC
+            padding: CryptoJS.pad.Pkcs7,
+            mode: CryptoJS.mode.CBC
         });
         return salt.toString() + initialVector.toString() + encrypted.toString();
-    };
-    SimpleCrypto.prototype.decrypt = function (string, enc) {
-        if (enc === void 0) { enc = crypto_js_1.default.enc.Utf8; }
-        var salt = crypto_js_1.default.enc.Hex.parse(string.substr(0, 32));
-        var initialVector = crypto_js_1.default.enc.Hex.parse(string.substr(32, 32));
-        var encrypted = string.substring(64);
-        var key = crypto_js_1.default.PBKDF2(this.secret, salt, {
+    }
+    decrypt(string, enc = CryptoJS.enc.Utf8) {
+        let salt = CryptoJS.enc.Hex.parse(string.substr(0, 32));
+        let initialVector = CryptoJS.enc.Hex.parse(string.substr(32, 32));
+        let encrypted = string.substring(64);
+        let key = CryptoJS.PBKDF2(this.secret, salt, {
             keySize: this.keySize / 32,
             iterations: this.iterations
         });
-        var decrypted = crypto_js_1.default.AES.decrypt(encrypted, key, {
+        let decrypted = CryptoJS.AES.decrypt(encrypted, key, {
             iv: initialVector,
-            padding: crypto_js_1.default.pad.Pkcs7,
-            mode: crypto_js_1.default.mode.CBC
+            padding: CryptoJS.pad.Pkcs7,
+            mode: CryptoJS.mode.CBC
         });
         return decrypted.toString(enc);
-    };
-    SimpleCrypto.prototype.encryptObject = function (object) {
+    }
+    encryptObject(object) {
         return this.encrypt(JSON.stringify(object));
-    };
-    SimpleCrypto.prototype.decryptObject = function (string) {
+    }
+    decryptObject(string) {
         return JSON.parse(this.decrypt(string));
-    };
-    SimpleCrypto.generateRandom = function (length, wordArray) {
-        if (length === void 0) { length = 128; }
-        if (wordArray === void 0) { wordArray = false; }
-        var random = crypto_js_1.default.lib.WordArray.random(length / 8);
+    }
+    static generateRandom(length = 128, wordArray = false) {
+        let random = CryptoJS.lib.WordArray.random(length / 8);
         return (wordArray) ? random : random.toString();
-    };
-    return SimpleCrypto;
-}());
-exports.default = SimpleCrypto;
+    }
+}
