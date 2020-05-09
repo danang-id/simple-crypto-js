e72e585750a81bfea5f9a1eb3d0d8be79ec64b27
Danang Galuh Tegar Prasetyo <danang.galuh.t@outlook.com>
2020-05-10 06:31:29 +0700

    Test: Conforms advanced cryptography APIs
    Added test for advanced functions, also better coverage for testing.

--- test/Cryptographic.test.ts
+++ test/Cryptographic.test.ts
@@ -1,77 +1,80 @@
-import chai from "chai";
+import {expect} from "chai"
 
-import SimpleCrypto from "../src/SimpleCrypto";
+import SimpleCrypto from "../src/SimpleCrypto"
 
-const expect = chai.expect;
-const secretKey = SimpleCrypto.generateRandom();
-const instance = new SimpleCrypto(secretKey.toString());
+const secretKey = SimpleCrypto.generateRandomWordArray()
+const instance = new SimpleCrypto(secretKey)
 
 const object = {
   SimpleCrypto: "is great.",
   You: "should try it!"
-};
-const string = "SimpleCrypto is great.";
-const number = 19960404;
-const boolean = false;
+}
+const string = "SimpleCrypto is great."
+const number = 19960404
+const boolean = false
 
-const cipherTextFromObject = instance.encrypt(object);
-const cipherTextFromString = instance.encrypt(string);
-const cipherTextFromNumber = instance.encrypt(number);
-const cipherTextFromBoolean = instance.encrypt(boolean);
+const cipherTextFromObject = instance.encryptObject(object)
+const cipherTextFromString = instance.encrypt(string)
+const cipherTextFromNumber = instance.encrypt(number)
+const cipherTextFromBoolean = instance.encrypt(boolean)
 
-const plainExpectsObject = instance.decrypt(cipherTextFromObject, true);
-const plainExpectsString = instance.decrypt(cipherTextFromString);
-const plainExpectsNumber = instance.decrypt(cipherTextFromNumber);
-const plainExpectsBoolean = instance.decrypt(cipherTextFromBoolean);
+const plainExpectsObject = instance.decryptObject(cipherTextFromObject)
+const plainExpectsString = instance.decrypt(cipherTextFromString, SimpleCrypto.encoders.Utf8)
+const plainExpectsNumber = instance.decrypt(cipherTextFromNumber, false)
+const plainExpectsBoolean = instance.decrypt(cipherTextFromBoolean)
 
 describe("Cryptographic: Encryption", () => {
+
   it("should be able to encrypt from object", () => {
-    expect(cipherTextFromObject).to.be.a("string");
-    expect(cipherTextFromObject).to.have.length(216);
-    expect(cipherTextFromObject.substr(150, 2)).to.be.equals("==");
-  });
+    expect(cipherTextFromObject).to.be.a("string")
+    expect(cipherTextFromObject).to.have.length(216)
+    expect(cipherTextFromObject.substr(149, 1)).not.to.be.equals("=")
+    expect(cipherTextFromObject.substr(150, 2)).to.be.equals("==")
+  })
 
   it("should be able to encrypt from string", () => {
-    expect(cipherTextFromString).to.be.a("string");
-    expect(cipherTextFromString).to.have.length(172);
-    expect(cipherTextFromString.substr(107, 1)).to.be.equals("=");
-  });
+    expect(cipherTextFromString).to.be.a("string")
+    expect(cipherTextFromString).to.have.length(172)
+    expect(cipherTextFromString.substr(106, 1)).not.to.be.equals("=")
+    expect(cipherTextFromString.substr(107, 1)).to.be.equals("=")
+  })
 
   it("should be able to encrypt from number", () => {
-    expect(cipherTextFromNumber).to.be.a("string");
-    expect(cipherTextFromNumber).to.have.length(152);
-    expect(cipherTextFromNumber.substr(86, 2)).to.be.equals("==");
-  });
+    expect(cipherTextFromNumber).to.be.a("string")
+    expect(cipherTextFromNumber).to.have.length(152)
+    expect(cipherTextFromNumber.substr(85, 1)).not.to.be.equals("=")
+    expect(cipherTextFromNumber.substr(86, 2)).to.be.equals("==")
+  })
 
   it("should be able to encrypt from boolean", () => {
-    expect(cipherTextFromBoolean).to.be.a("string");
-    expect(cipherTextFromBoolean).to.have.length(152);
-    expect(cipherTextFromBoolean.substr(86, 2)).to.be.equals("==");
-  });
-});
+    expect(cipherTextFromBoolean).to.be.a("string")
+    expect(cipherTextFromBoolean).to.have.length(152)
+    expect(cipherTextFromBoolean.substr(85, 1)).not.to.be.equals("=")
+    expect(cipherTextFromBoolean.substr(86, 2)).to.be.equals("==")
+  })
+
+})
 
 describe("Cryptographic: Decryption", () => {
+
   it("should be able to decrypt to object", () => {
-    expect(plainExpectsObject).to.be.a("object");
-    expect(plainExpectsObject).to.be.eql(object);
-  });
+    expect(plainExpectsObject).to.be.a("object")
+    expect(plainExpectsObject).to.be.eql(object)
+  })
 
   it("should be able to decrypt to string", () => {
-    expect(plainExpectsString).to.be.a("string");
-    expect(plainExpectsString).to.be.eql(string);
-  });
+    expect(plainExpectsString).to.be.a("string")
+    expect(plainExpectsString).to.be.eql(string)
+  })
 
   it("should be able to decrypt to number", () => {
-    expect(plainExpectsNumber).to.be.a("string");
-    expect(plainExpectsNumber).to.be.eql(number.toString());
-    expect(parseInt(plainExpectsNumber.toString())).to.be.a("number");
-    expect(parseInt(plainExpectsNumber.toString())).to.be.eql(number);
-  });
+    expect(plainExpectsNumber).to.be.a("number")
+    expect(plainExpectsNumber).to.be.eql(number)
+  })
 
   it("should be able to decrypt to boolean", () => {
-    expect(plainExpectsBoolean).to.be.a("string");
-    expect(plainExpectsBoolean).to.be.eql(boolean.toString());
-    expect(plainExpectsNumber === "true").to.be.a("boolean");
-    expect(plainExpectsNumber === "true").to.be.eql(boolean);
-  });
-});
+    expect(plainExpectsBoolean).to.be.a("boolean")
+    expect(plainExpectsBoolean).to.be.eql(boolean)
+  })
+
+})
--- test/RandomGenerator.test.ts
+++ test/RandomGenerator.test.ts
@@ -1,50 +1,51 @@
-import chai from "chai";
+import {expect} from "chai"
 
-import SimpleCrypto from "../src/SimpleCrypto";
+import SimpleCrypto from "../src/SimpleCrypto"
 
-const expect = chai.expect;
 const keyLength = {
   default: 128,
   custom: 256
-};
+}
 
 describe("Random Generator", () => {
+
   it("expects random string", () => {
-    const random = SimpleCrypto.generateRandom();
-    expect(random).to.be.a("string");
-  });
+    const random = SimpleCrypto.generateRandom()
+    expect(random).to.be.a("string")
+  })
 
   it("expects random string (using default key length)", () => {
-    const usedKeyLength = keyLength.default;
-    const random = SimpleCrypto.generateRandom(usedKeyLength);
-    expect(random).to.be.a("string");
-    expect(random).to.have.lengthOf(usedKeyLength / 4);
-  });
+    const usedKeyLength = keyLength.default
+    const random = SimpleCrypto.generateRandomString(usedKeyLength)
+    expect(random).to.be.a("string")
+    expect(random).to.have.lengthOf(usedKeyLength / 4)
+  })
 
   it("expects random string (using customised key length)", () => {
-    const usedKeyLength = keyLength.custom;
-    const random = SimpleCrypto.generateRandom(usedKeyLength);
-    expect(random).to.be.a("string");
-    expect(random).to.have.lengthOf(usedKeyLength / 4);
-  });
+    const usedKeyLength = keyLength.custom
+    const random = SimpleCrypto.generateRandomString(usedKeyLength)
+    expect(random).to.be.a("string")
+    expect(random).to.have.lengthOf(usedKeyLength / 4)
+  })
 
   it("expects random WordArray (using default key length)", () => {
-    const usedKeyLength = keyLength.default;
-    const random: any = SimpleCrypto.generateRandom(usedKeyLength, true);
-    expect(random).to.be.a("object");
-    expect(random).to.have.keys(["words", "sigBytes"]);
-    expect(random["words"]).to.be.a("array");
-    expect(random["words"].length).to.be.equals(usedKeyLength / 32);
-    expect(random["sigBytes"]).to.be.equals(usedKeyLength / 8);
-  });
+    const usedKeyLength = keyLength.default
+    const random: any = SimpleCrypto.generateRandomWordArray(usedKeyLength)
+    expect(random).to.be.a("object")
+    expect(random).to.have.keys(["words", "sigBytes"])
+    expect(random["words"]).to.be.a("array")
+    expect(random["words"].length).to.be.equals(usedKeyLength / 32)
+    expect(random["sigBytes"]).to.be.equals(usedKeyLength / 8)
+  })
 
   it("expects random WordArray (using customised key length)", () => {
-    const usedKeyLength = keyLength.custom;
-    const random: any = SimpleCrypto.generateRandom(usedKeyLength, true);
-    expect(random).to.be.a("object");
-    expect(random).to.have.keys(["words", "sigBytes"]);
-    expect(random["words"]).to.be.a("array");
-    expect(random["words"].length).to.be.equals(usedKeyLength / 32);
-    expect(random["sigBytes"]).to.be.equals(usedKeyLength / 8);
-  });
-});
+    const usedKeyLength = keyLength.custom
+    const random: any = SimpleCrypto.generateRandomWordArray(usedKeyLength)
+    expect(random).to.be.a("object")
+    expect(random).to.have.keys(["words", "sigBytes"])
+    expect(random["words"]).to.be.a("array")
+    expect(random["words"].length).to.be.equals(usedKeyLength / 32)
+    expect(random["sigBytes"]).to.be.equals(usedKeyLength / 8)
+  })
+
+})
